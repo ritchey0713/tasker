8dba260ca428fc95c1bde2a8b30af79f450b049a
Dj <ritchey0713@gmail.com>
2020-02-06 13:58:57 -0500

    typo found in url

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,13 @@
 const mongoDb = require('mongodb')
 const MongoClient = mongoDb.MongoClient
 
-const connectionURL = "mongo://127.0.0.1:27017"
+const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = 'tasker-dev'
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-  
+  if(error) {
+    return console.log("connect failed:", error)
+  }
+
+  console.log("connected successfully!")
 })
