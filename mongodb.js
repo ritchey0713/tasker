91089405b9baceead6da6c8183c0e754d41438fe
Dj <ritchey0713@gmail.com>
2020-02-06 13:55:28 -0500

    set up connect

--- mongodb.js
+++ mongodb.js
@@ -2,4 +2,8 @@ const mongoDb = require('mongodb')
 const MongoClient = mongoDb.MongoClient
 
 const connectionURL = "mongo://127.0.0.1:27017"
-const database = 'tasker-dev'
+const databaseName = 'tasker-dev'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  
+})
