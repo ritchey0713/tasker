44d2b10694607dddceca8dda21560d7594d56b0c
Dj <ritchey0713@gmail.com>
2020-02-07 17:57:56 -0500

    remove code

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const connectionURL = 'mongodb://127.0.0.1:27017/task-lister'
 
@@ -9,45 +8,6 @@ mongoose.connect(connectionURL, {
   useNewUrlParser: true
 })
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if(value < 0) {
-        throw new Error("age must be a positive number")
-      }
-    }
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if(!validator.isEmail(value)) {
-        throw new Error("Invalid Email")
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    validate(value) {
-      if(value.length < 6 ) {
-        throw new Error("password not not enough!")
-      } else if(value.includes("password")) {
-        throw new Error("Don't include 'password' in your password!")
-      }
-    }
-  }
-})
 
 const Task= mongoose.model("Task", {
   description: {
@@ -62,29 +22,3 @@ const Task= mongoose.model("Task", {
   }
 })
 
-// const me = new User({
-//   name: "    Dave       ",
-//   email: "EMAIL@EMAIL.COM",
-//   password: "test123123"
-// })
-
-// me.save()
-//   .then((res) => {
-//     console.log("SAVED", res)
-//   })
-//   .catch((err) => {
-//     console.log("SAVE ERROR", err)
-//   })
-
-  const task = new Task({
-    description: "Tester                    123",
-    completed: false
-  })
-
-  task.save()
-    .then(() => {
-      console.log("SAVED", task)
-    })
-    .catch((error) => {
-      console.log('TASK NOT SAVED', error)
-    })
