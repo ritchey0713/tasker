c1a080ab3b28e7b12a464e7f0ba72c7d5a1e705b
Dj <ritchey0713@gmail.com>
2020-02-07 17:26:47 -0500

    test post route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.post('/contractors', (req, res) => {
+  console.log(req.body)
   res.send("testing")
 })
 
