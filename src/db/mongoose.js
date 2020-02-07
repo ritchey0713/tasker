44ed85d2bdcd680febebd97dd3ab06dc2da8f78c
Dj <ritchey0713@gmail.com>
2020-02-07 13:50:06 -0500

    build user and task models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,1 +1,50 @@
 const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-lister', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+
+const User = mongoose.model("User", {
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  } 
+})
+
+const Task= mongoose.model("Task", {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
+})
+
+const me = new User({
+  name: "me",
+  age: "true"
+})
+
+// me.save()
+//   .then((res) => {
+//     console.log("SAVED", res)
+//   })
+//   .catch((err) => {
+//     console.log("SAVE ERROR", err)
+//   })
+
+  const task = new Task({
+    description: "Test task",
+    completed: false
+  })
+
+  task.save()
+    .then(() => {
+      console.log("SAVED", task)
+    })
+    .catch((error) => {
+      console.log('TASK NOT SAVED', error)
+    })
