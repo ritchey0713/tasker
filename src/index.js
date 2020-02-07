76999d2772256eb933f51818ff06e604e33094b2
Dj <ritchey0713@gmail.com>
2020-02-07 17:07:44 -0500

    test contractor post

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const express = require('express')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.post('/contractors', (req, res) => {
+  res.send("testing")
+})
+
+app.listen(port, () => {
+  console.log(`server loaded at ${port}`)
+})
