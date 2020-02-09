df6401992d0a513da1ec60d0e8df6a9dc3f6f205
Dj <ritchey0713@gmail.com>
2020-02-09 13:38:44 -0500

    remove warning

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,7 +5,8 @@ const connectionURL = 'mongodb://127.0.0.1:27017/task-lister'
 mongoose.connect(connectionURL, {
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 })
 
 
