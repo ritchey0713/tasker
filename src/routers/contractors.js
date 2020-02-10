381a1649ef5bf1c6a4061b38323ab470e13533c8
Dj <ritchey0713@gmail.com>
2020-02-10 12:15:19 -0500

    build contractor router

---
+++ src/routers/contractors.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Contractor = require("../models/contractor")
+const router = express.Router()
+
+router.post('/contractors', async(req, res) => {
+  const contractor = new Contractor(req.body)
+
+  try {
+    await contractor.save()
+    res.status(201).send(contractor)
+  } catch(err) {
+    res.status(400).send(err)
+  }
+
+})
+
+router.get("/contractors", async(req, res) => {
+  try {
+    const contractors = await Contractor.find({  })
+    res.status(200).send(contractors)
+  }catch(err) {
+    res.status(400).send(err)
+  }
+})
+
+router.get("/contractors/:id", async(req, res) => {
+  const _id = req.params.id
+
+  try {
+    const contractor = await Contractor.findById(_id)
+    if(!contractor){
+      return res.status(404).send()
+    }
+    res.send(contractor)
+  }catch(err) {
+    res.status(500).send()
+  }
+
+})
+
+router.patch("/contractors/:id", async(req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdate = ["name", "age", "email", "password"]
+  const isValidUpdates = updates.every((update) => {
+    return allowedUpdate.includes(update)
+  })
+
+  if(!isValidUpdates) {
+    return res.status(400).send({ error: "Invalid updates"})
+  }
+
+  try{
+    const contractor = await Contractor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if(!contractor) {
+      return res.status(404).send()
+    }
+    res.status(200).send(contractor)
+  } catch(err){
+    res.status(400).send(err)
+  }
+})
+
+router.delete("/contractors/:id", async(req, res) => {
+  try {
+    const contractor = await Contractor.findByIdAndDelete(req.params.id)
+
+    if(!contractor) {
+      return res.status(404),send()
+    }
+
+    res.send(contractor)
+  }catch(e) {
+    res.status(500).send()
+  }
+})
+
+module.exports = router
