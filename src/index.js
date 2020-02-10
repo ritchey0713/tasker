6e82182b08fe12ca1701d16a495129d681bde89e
Dj <ritchey0713@gmail.com>
2020-02-10 12:15:09 -0500

    remove contractor from app file

--- src/index.js
+++ src/index.js
@@ -2,86 +2,15 @@ const express = require('express')
 require('./db/mongoose')
 const Contractor = require("./models/contractor")
 const Task = require('./models/task')
+const contractorRouter = require("./routers/contractors.js")
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(contractorRouter)
 
-const router = new express.Router()
 
-app.post('/contractors', async(req, res) => {
-  const contractor = new Contractor(req.body)
-
-  try {
-    await contractor.save()
-    res.status(201).send(contractor)
-  } catch(err) {
-    res.status(400).send(err)
-  }
-
-})
-
-app.get("/contractors", async(req, res) => {
-  try {
-    const contractors = await Contractor.find({  })
-    res.status(200).send(contractors)
-  }catch(err) {
-    res.status(400).send(err)
-  }
-})
-
-app.get("/contractors/:id", async(req, res) => {
-  const _id = req.params.id
-
-  try {
-    const contractor = await Contractor.findById(_id)
-    if(!contractor){
-      return res.status(404).send()
-    }
-    res.send(contractor)
-  }catch(err) {
-    res.status(500).send()
-  }
-
-})
-
-app.patch("/contractors/:id", async(req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdate = ["name", "age", "email", "password"]
-  const isValidUpdates = updates.every((update) => {
-    return allowedUpdate.includes(update)
-  })
-
-  if(!isValidUpdates) {
-    return res.status(400).send({ error: "Invalid updates"})
-  }
-
-  try{
-    const contractor = await Contractor.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
-    if(!contractor) {
-      return res.status(404).send()
-    }
-    res.status(200).send(contractor)
-  } catch(err){
-    res.status(400).send(err)
-  }
-})
-
-app.delete("/contractors/:id", async(req, res) => {
-  try {
-    const contractor = await Contractor.findByIdAndDelete(req.params.id)
-
-    if(!contractor) {
-      return res.status(404),send()
-    }
-
-    res.send(contractor)
-  }catch(e) {
-    res.status(500).send()
-  }
-})
 
 app.post('/tasks', async(req, res) => {
 
