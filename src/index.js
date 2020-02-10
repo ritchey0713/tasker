ebc30464fa7c1fbf4cb90f5e6363a0ea4ac17ea3
Dj <ritchey0713@gmail.com>
2020-02-09 23:09:53 -0500

    async index for tasks

--- src/index.js
+++ src/index.js
@@ -55,14 +55,14 @@ app.post('/tasks', async(req, res) => {
   
 })
 
-app.get("/tasks", (req, res) => {
-  Task.find({  })
-  .then((tasks) => {
+app.get("/tasks", async(req, res) => {
+  try {
+    const tasks = await Task.find({ })
     res.send(tasks)
-  })
-  .catch((error) => {
+  }catch(err) {
     res.status(500).send()
-  })
+  }
+
 })
 
 app.get("/tasks/:id", async(req, res) => {
