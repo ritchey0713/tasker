99b8f6f245fc25bd33e57c57ed0ed59af87fb379
Dj <ritchey0713@gmail.com>
2020-02-11 16:30:03 -0500

    test jwt

--- src/index.js
+++ src/index.js
@@ -13,3 +13,14 @@ app.use(contractorRouter, taskRouter)
 app.listen(port, () => {
   console.log(`server loaded at ${port}`)
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+  const token = jwt.sign({ _id: "abc123" }, 'thisismynewtoken', {expiresIn: '0 second'})
+  console.log(token)
+  const data = jwt.verify(token, 'thisismynewtoken')
+  console.log(data)
+}
+
+myFunction()
