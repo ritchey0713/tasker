9734fcaa1ce51a0ac3274788978a60ef9a59708c
Dj <ritchey0713@gmail.com>
2020-02-11 23:39:42 -0500

    wire up middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,14 @@ const Contractor = require("../models/contractor.js")
 
 const auth = async (req, res, next) => {
   try {
-
+    const token = req.header('Authorization').replace('Bearer ', "")
+    const decoded = jwt.verify(token, "thisismynewtoken")
+    const contractor = await Contractor.findOne({ _id: decoded._id, "tokens.token": token })
+    if(!contractor) {
+      throw new Error()
+    }
+    req.contractor = contractor
+    next()
   }catch(err) {
     res.status(401).send({ error: "Please authenticate" })
   }
