b43c91eb70efd6f1d4a02453c6cc5b05602b754d
Dj <ritchey0713@gmail.com>
2020-02-12 13:47:36 -0500

    check user before fetching task

--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -27,13 +27,14 @@ router.get("/tasks", async(req, res) => {
 
 })
 
-router.get("/tasks/:id", async(req, res) => {
+router.get("/tasks/:id", auth, async(req, res) => {
   const _id = req.params.id 
 
   try {
-    const task = await Task.findById(_id)
+    //const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id, contractor: req.contractor._id })
     if(!task) {
-      return res.status(404).send()
+      return res.status(404).send({error: "No task found!"})
     }
     res.send(task)
   }catch(err){
