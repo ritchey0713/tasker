5ab160e272b7e7b2f54a4407531a46778bb80101
Dj <ritchey0713@gmail.com>
2020-02-11 22:44:14 -0500

    maint wares

--- src/index.js
+++ src/index.js
@@ -7,8 +7,26 @@ const taskRouter = require("./routers/tasks.js")
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//   console.log(req.method, req.path)
+//   if(req.method === "GET") {
+//     res.send("GET requests are disabled")
+//   } else {
+//     next()
+//   }
+  
+// })
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Under maintenance!!")
+// })
+
 app.use(express.json())
 app.use(contractorRouter, taskRouter)
+
+// without middleware => new request => run route handler
+
+// with middleware => new request => do something => run route handler
 
 app.listen(port, () => {
   console.log(`server loaded at ${port}`)
