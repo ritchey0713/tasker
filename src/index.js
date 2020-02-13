8dcd8c19c94a1bc7427b0772c8d8dcc77cb2ecb7
Dj <ritchey0713@gmail.com>
2020-02-13 15:49:27 -0500

    remove comments

--- src/index.js
+++ src/index.js
@@ -28,33 +28,7 @@ app.use(contractorRouter, taskRouter)
 
 // with middleware => new request => do something => run route handler
 
-// const multer = require("multer")
-// const upload = multer({
-//   dest: "images",
-//   limits: {
-//     fileSize: 1000000
-//   },
-//   fileFilter(req, file, cb) {
-//     if(!file.originalname.match(/\.(doc|docx)$/)) {
-//       return cb(new Error("Must be a Word document"))
-//     }
-//     cb(undefined, true)
-//   }
-// })
-
-// const errorMiddleWare = (req, res, next) => {
-//   throw new Error("from my middleware")
-// }
-
-// app.post("/upload", upload.single("upload"), (req, res) => {
-//   res.send()
-// }, (error, req, res, next) => {
-//   res.status(400).send({ error: error.message })
-// })
-
 
 app.listen(port, () => {
   console.log(`server loaded at ${port}`)
 })
-
-
