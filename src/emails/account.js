e9871cca9b2ad400c06ce8beae875a164ab8f211
Dj <ritchey0713@gmail.com>
2020-02-14 11:40:45 -0500

    add emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -3,3 +3,26 @@ const sgMail = require('@sendgrid/mail')
 
 sgMail.setApiKey(secretKey)
 
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'ritchey0713@gmail.com',
+    subject: "Thanks for signing up!",
+    text: `Hey there ${name}! Thanks for signing up, let me know how you like the application!`,
+    // html: `<header>Hey there ${name}!</header><br/><p>Thanks for signing up, let me know how you like the application!</p>`
+  })
+}
+
+const cancelEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "ritchey0713@gmail.com",
+    subject: "Sorry to see you leave us",
+    text: `Hey ${name}, I'm sorry you have cancelled your account, is there anything we can do to bring you back in the future? Have a wonderful day!`
+  })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  cancelEmail
+}
