8dc5fa8f9b2508abe888c0d0d63912da70567823
Dj <ritchey0713@gmail.com>
2020-02-14 13:30:03 -0500

    set up env

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectionURL = 'mongodb://127.0.0.1:27017/task-lister'
+const connectionURL = process.env.URL
 
 mongoose.connect(connectionURL, {
   useUnifiedTopology: true,
