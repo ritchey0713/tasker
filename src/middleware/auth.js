0200d01037b8b2e10385f91584308a44b86dbd43
Dj <ritchey0713@gmail.com>
2020-02-14 13:30:41 -0500

    set up token

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const Contractor = require("../models/contractor.js")
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', "")
-    const decoded = jwt.verify(token, "thisismynewtoken")
+    const decoded = jwt.verify(token, process.env.JWT_TOKEN)
     const contractor = await Contractor.findOne({ _id: decoded._id, "tokens.token": token })
     if(!contractor) {
       throw new Error()
