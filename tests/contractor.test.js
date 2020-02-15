e6b6a2d2b2aa6154d85a953d093d4e64450c5219
Dj <ritchey0713@gmail.com>
2020-02-14 22:02:11 -0500

    add login tests

--- tests/contractor.test.js
+++ tests/contractor.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require("../src/app.js")
+const Contractor = require("../src/models/contractor.js")
+
+const contractorOne = {
+  name: "Mike",
+  email: "Mike@mike.com",
+  password: "Mike123"
+}
+
+beforeEach(async () => {
+  await Contractor.deleteMany()
+  await new Contractor(contractorOne).save()
+})
 
 test("Should signup new contractor", async () => {
   await request(app).post("/contractors").send({
@@ -8,3 +20,18 @@ test("Should signup new contractor", async () => {
     password:"MySecret111!"
   }).expect(201)
 })
+
+test("Should login a user", async () => {
+  await request(app).post("/contractors/login").send({
+    email: contractorOne.email,
+    password: contractorOne.password
+  }).expect(200)
+})
+
+test("Should not login a non-existent user", async () => {
+  await request(app).post("/contractors/login").send({
+    email: "Bad User",
+    password: "NotSoSecret"
+  }).expect(400)
+})
+
