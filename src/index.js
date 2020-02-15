e554820bb419d4a9fb9d9bf60bd22513361ae457
Dj <ritchey0713@gmail.com>
2020-02-14 21:25:37 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-
-const contractorRouter = require("./routers/contractors.js")
-const taskRouter = require("./routers/tasks.js")
-
-const app = express()
+const app = require("./app.js")
 const port = process.env.PORT
 
-// app.use((req, res, next) => {
-//   console.log(req.method, req.path)
-//   if(req.method === "GET") {
-//     res.send("GET requests are disabled")
-//   } else {
-//     next()
-//   }
-  
-// })
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Under maintenance!!")
-// })
-
-app.use(express.json())
-
-//allows use of middleware
-app.use(contractorRouter, taskRouter)
-
-// without middleware => new request => run route handler
-
-// with middleware => new request => do something => run route handler
 
 
 app.listen(port, () => {
