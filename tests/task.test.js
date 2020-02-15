f7b89c82706338886069eb79aad070e837a9796d
Dj <ritchey0713@gmail.com>
2020-02-15 18:54:37 -0500

    add requires

---
+++ tests/task.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest")
+const jtw = require("jsonwebtoken")
+const mongoose = require("mongoose")
+const Task = require("../src/models/task.js")
+const app = require("../src/app.js")
+
+test("Should create a new task", async () => {
+
+})
