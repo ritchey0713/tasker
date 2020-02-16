221de930c04674d564568d8c5e2b0fe8f3598878
Dj <ritchey0713@gmail.com>
2020-02-15 19:32:55 -0500

    check contractor for tasks

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest")
 const Task = require("../src/models/task.js")
+const Contractor = require("../src/models/contractor.js")
 const app = require("../src/app.js")
 const { contractorOne, contractorOneId, setupDb, taskOne, taskTwo } = require("./fixtures/db.js")
 
@@ -17,5 +18,14 @@ test("Should create a new task",  async () => {
   const task = await Task.findById(resp.body._id)
   expect(task).not.toBeNull()
   expect(task.completed).toBe(false)
+})
 
+test("Should get tasks of the signed in user", async () => {
+  const resp =  await request(app).get("/tasks")
+  .set("Authorization", `Bearer ${contractorOne.tokens[0].token}`)
+  .expect(200)
+
+  const contractor = await Contractor.findById(contractorOneId).populate("tasks")
+
+  expect(contractor.tasks.length).toEqual(resp.body.length)
 })
